053b25eaf5738e231f0545686755b74c2414d42c
calirojas <juancarlosr@msn.com>
2017-12-11 00:57:45 -0600

    vue-console files

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Basic JavaScript Console for Mobile Devices, as a Vue.js component.
 ## What is vue-console?
 vue-console is a very basic JavaScript console for mobile devices. It means, you'll be able to execute JavaScript code directly in your mobile browser and get error messages generated in run-time.
 
-This is a component created using Vue.js, so you need Vue.js in order to use it. Also, the tiny UI is using Bootstrap.
+This is a component created using Vue.js, so you need Vue.js in order to use it. Also, the tiny UI uses Bootstrap.
 
 ## How to use vue-console?
 Simple. Add it to your page using a script tag, add the CSS file and, finally, place the &lt;vue-console/&gt; component inside your code.
---
+++ js/vue-console.js
@@ -0,0 +1,154 @@
+/*
+	vue-console
+	A Basic JavaScript Console for Mobile Devices
+
+	Released under MIT License
+	Copyright (c) 2017 Cali Rojas
+	https://github.com/calirojas506/vue-console
+*/
+
+Vue.component('vue-console', {
+	props: {
+		isVisible: {
+			required: false,
+			default: true,
+			type: Boolean
+		}
+	},
+	template: `
+		<div class="c-vue-console" :class="{hidden: !isVisible}">
+			<div class="container-fluid">
+				<div class="c-vue-console__header row">
+					<div class="col-xs-12">
+						<div class="panel panel-primary">
+							<div class="panel-heading" @click="togglePanelBody">
+								<h3 class="panel-title">
+									{{componentName}}
+									<span class="pull-right close"
+									@click="toggleVisibility">&times;</span>
+								</h3>
+								<span class="small">{{componentDescription}}</span>
+							</div>
+							<div class="panel-body" v-show="logs.length" ref="panelBody"
+							:class="{'hidden' : !panelBodyIsVisible}">
+								<ul class="list-unstyled small">
+									<li v-for="log in logs" :class="{'text-muted': log.message == 'undefined'}">
+										<span class="label" :class="'label-' + log.style">&nbsp;</span>
+										{{log.message}}
+									</li>
+								</ul>
+							</div>
+							<div class="panel-footer">
+								<form @submit.prevent="execute" @reset.prevent="resetForm">
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-btn">
+												<button class="btn btn-danger btn-sm" type="reset">&times;</button>
+											</span>
+											<input type="text" class="form-control input-sm"
+											autocapitalize="none" v-model="commandInput" ref="commandInput">
+											<span class="input-group-btn">
+												<button class="btn btn-primary btn-sm" type="submit">&check;</button>
+											</span>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	`,
+	data(){
+		return {
+			componentName: 'vue-console',
+			componentDescription: 'Basic JavaScript Console for Mobile Devices',
+			commandInput: '',
+			logs: [],
+			panelBodyIsVisible: true
+		}
+	},
+	methods: {
+		toggleVisibility(){
+			this.isVisible = !this.isVisible;
+		},
+		togglePanelBody(){
+			this.panelBodyIsVisible = !this.panelBodyIsVisible;
+		},
+		execute(){
+			if(this.commandInput.trim() =='') return false;
+
+			if(!this.commandInput.startsWith('console.')){
+				this.logs.push({
+					message: (window.eval(this.commandInput) ? eval(this.commandInput) : 'undefined'),
+					style: 'primary'
+				});
+			}else{
+				window.eval(this.commandInput);
+			}
+
+			this.selectCommandInput();
+		},
+		consoleLog(...message){
+			this.logs.push({
+				message: message.join(' '),
+				style: 'primary'
+			});
+		},
+		consoleError(...message){
+			this.logs.push({
+				message: message.join(' '),
+				style: 'danger'
+			});
+		},
+		consoleInfo(...message){
+			this.logs.push({
+				message: message.join(' '),
+				style: 'info'
+			});
+		},
+		consoleWarn(...message){
+			this.logs.push({
+				message: message.join(' '),
+				style: 'warning'
+			});
+		},
+		consoleClear(){
+			this.logs = [];
+		},
+		init(){
+			console.log = this.consoleLog;
+			console.error = this.consoleError;
+			console.warn = this.consoleWarn;
+			console.clear = this.consoleClear;
+			console.info = this.consoleInfo;
+
+			window.onerror = null;
+			let _this = this;
+			window.onerror = function(message, url, line, column, errObject){
+				_this.consoleError(message, url, line + ':' + column);
+				_this.selectCommandInput();
+			}
+		},
+		resetForm(){
+			this.commandInput = '';
+			this.$refs.commandInput.focus();
+		},
+		selectCommandInput(){
+			let input = this.$refs.commandInput;
+
+			input.setSelectionRange(0, input.value.length);
+		}
+	},
+	created(){
+		this.init();
+	},
+	watch: {
+		logs(){
+			let panelBody = this.$refs.panelBody;
+
+			panelBody.scrollTop = panelBody.scrollHeight;
+		}
+	}
+});
